a34e703fb77c44ccfad6299b677f9dae33427a9c
Helmut Granda <contact@helmutgranda.com>
2016-11-23 22:30:26 -0600

    Displaying the Single Photo Component - 13 of 20

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Comments = React.createClass( {
+    render() {
+        return(
+            <div className="comments">
+                I am the comments
+            </div>
+        )
+        
+    }
+});
+
+export default Comments;
