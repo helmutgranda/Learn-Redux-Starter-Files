53d20702396d2e0cba2ea8e2385994be7c45686f
Helmut Granda <contact@helmutgranda.com>
2016-11-23 22:56:32 -0600

    Redux Reducer Composition - 16 of 20

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -9,7 +9,7 @@ const Comments = React.createClass( {
                 <p>
                     <strong>{comment.user}</strong>
                     {comment.text}
-                    <button className="remove-comment">&times;</button>
+                    <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
                 </p>
             </div>
         )
@@ -21,6 +21,7 @@ const Comments = React.createClass( {
         const author = this.refs.author.value;
         const comment = this.refs.comment.value;
         this.props.addComment(postId, author, comment);
+        this.refs.commentForm.reset();  
     },
 
     render() {
@@ -28,7 +29,7 @@ const Comments = React.createClass( {
             <div className="comments">
                 {this.props.postComments.map(this.renderComment)}
                 <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
-                    <input type="text" ref="autor" placeholder="autor"/>
+                    <input type="text" ref="author" placeholder="author"/>
                     <input type="text" ref="comment" placeholder="comment"/>
                     <input type="submit" hidden />
                 </form>
